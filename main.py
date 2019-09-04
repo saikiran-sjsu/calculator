f99b24fa71082e0707de9b5a90cdec95a5568356
Saikiran Vaidhyanathan <saikiran.vaidhyanathan@sjsu.edu>
2019-09-04 12:24:53 -0700

    third line testing

--- main.py
+++ main.py
@@ -2,4 +2,5 @@ def main():
     print('The programs divides numbers')
     num1 = input('Please enter first number')
     num2 = input('Please enter the second number')
+    num3 = input('hi')	
 main()
